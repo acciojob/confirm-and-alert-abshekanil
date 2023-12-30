d4cbf052ee99e8818ac2befdb4153a4e94ac859a
Abhishek Anil <141896050+abshekanil@users.noreply.github.com>
2023-12-30 10:52:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function confirmAndAlert()
+	{
+		let result = confirm("Do you want to proceed?");
+
+		if(result)
+		{
+			alert("You clicked OK. Proceeding...");
+		}else
+		{
+			alert("You clicked Cancel. Exiting...");
+		}
+	}
